4d8172501d9a8fff6710a39cd5a00c52759a7d1b
Samuel <samuvalcarcel@gmail.com>
2017-03-02 20:25:11 +0000

    Actualizando deploy.js y package.json

--- deploy.js
+++ deploy.js
@@ -1,6 +1,11 @@
 var ghpages = require('gh-pages');
 var path = require('path');
- 
-ghpages.publish(path.join(__dirname, '_book/*'), function(err) { 
-	console.log("Desplegando libro");
+
+ghpages.publish(path.join(__dirname, '_book'), function(err) {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    console.log("Desplegando libro");
+  }
  });
